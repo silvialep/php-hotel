829c7290e18ab3de68258b9c1d22bb31cfb06e4a
silvialep <silvia.lepore_sl@libero.it>
2023-04-19 14:27:00 +0200

    feat: corrections after morning lesson

--- index.php
+++ index.php
@@ -40,18 +40,72 @@ $hotels = [
 
 ];
 
-$hotelIndex = 0;
-$parkingOk = 'OK';
-$noParking = 'NO';
+// controllo del valore parcheggio, se non è settato restituisce false senza bloccare la pagina
+$parcheggio = $_GET['parcheggio'] ?? 'false';
 
-for($i = 0; $i < count($hotels); $i++) {
-    if($hotels[$i]['parking'] == true) {
-        $hotels[$i]['parking'] = $parkingOk;
-    } else {
-        $hotels[$i]['parking'] = $noParking;
+// if($parcheggio == 'checked') {
+//     $parcheggio = true;
+// }
+
+$filteredHotels = $hotels;
+
+
+if($parcheggio == 'OK') {
+    
+    $tempFilteredHotels = [];
+
+    foreach($hotels as $singleHotel) {
+        if($singleHotel['parking'] == true) {
+            $tempFilteredHotels[] = $singleHotel;
+        }
     }
+
+    $filteredHotels = $tempFilteredHotels;
+    
 }
-// $hotels[$hotelIndex]['parking'] = $parkingOk;
+
+if ($parcheggio == 'NO') {
+
+    $tempFilteredHotels = [];
+
+    foreach ($hotels as $singleHotel) {
+        if ($singleHotel['parking'] == false) {
+            $tempFilteredHotels[] = $singleHotel;
+        }
+    }
+
+    $filteredHotels = $tempFilteredHotels;
+
+} 
+
+// controllo del valore voto, se non è settato restituisce false senza bloccare la pagina
+$voto = $_GET['voto'] ?? 0;
+
+
+if($voto > 0) {
+
+    $tempFilteredHotels = [];
+
+    // $filteredVoteHotels = $filteredHotels;
+
+    foreach ($filteredHotels as $singleHotel) {
+        if ($singleHotel['vote'] == $voto) {
+            $tempFilteredHotels[] = $singleHotel;
+        }
+    }
+
+    $filteredHotels = $tempFilteredHotels;
+
+}
+
+// for($i = 0; $i < count($hotels); $i++) {
+//     if($hotels[$i]['parking'] == true) {
+//         $hotels[$i]['parking'] = $parkingOk;
+//     } else {
+//         $hotels[$i]['parking'] = $noParking;
+//     }
+// }
+
 // var_dump($hotels);
 
 ?>
@@ -60,6 +114,7 @@ for($i = 0; $i < count($hotels); $i++) {
 
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -68,78 +123,93 @@ for($i = 0; $i < count($hotels); $i++) {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
+
 <body>
+    <h1>Hotels</h1>
     <div style="display: inline-block; padding: 20px;">
         <h3>Scegli il parcheggio</h3>
-            <form action="index.php">
-            <input type="radio" name="parcheggio" value="OK" id="parcheggio">
-            <label for="parcheggio">OK</label>
-            <input type="radio" name="parcheggio" value="NO" id="parcheggio">
-            <label for="parcheggio">NO</label>
+        <form action="index.php" method="GET">
+            <input type="radio" name="parcheggio" value="OK" id="parcheggioOK">
+            <label for="parcheggioOK">OK</label>
+            <input type="radio" name="parcheggio" value="NO" id="parcheggioNO">
+            <label for="parcheggioNO">NO</label>
             <!-- <input type="submit" value="Submit"> -->
-        <!-- </form>
+            <!-- </form>
     </div>
     <div style="display: inline-block; padding: 20px;"> -->
             <h3>Scegli il voto</h3>
-        <!-- <form action="index.php"> -->
-            <input type="radio" name="voto" value="1" id="voto">
-            <label for="voto">1</label>
-            <input type="radio" name="voto" value="2" id="voto">
-            <label for="voto">2</label>
-            <input type="radio" name="voto" value="3" id="voto">
-            <label for="voto">3</label>
-            <input type="radio" name="voto" value="4" id="voto">
-            <label for="voto">4</label>
-            <input type="radio" name="voto" value="5" id="voto">
-            <label for="voto">5</label>
+            <!-- <form action="index.php"> -->
+            <input type="radio" name="voto" value="1" id="voto1">
+            <label for="voto1">1</label>
+            <input type="radio" name="voto" value="2" id="voto2">
+            <label for="voto2">2</label>
+            <input type="radio" name="voto" value="3" id="voto3">
+            <label for="voto3">3</label>
+            <input type="radio" name="voto" value="4" id="voto4">
+            <label for="voto4">4</label>
+            <input type="radio" name="voto" value="5" id="voto5">
+            <label for="voto5">5</label>
             <input type="submit" value="Submit">
         </form>
     </div>
+
+    <pre>
+        Output del form
+        Filtro parcheggio: <?php echo $parcheggio ?>
+        Filtro voto: <?php echo $voto ?>
+    </pre>
+
     <table class="table">
         <thead>
-            <tr>
-                <th scope="col">Nome Hotel</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Parcheggio</th>
-                <th scope="col">Voto</th>
-                <th scope="col">Distanza dal centro</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
             <?php
 
+            foreach ($hotels[0] as $hotelProperty => $value) {
+            ?>
 
-            foreach($hotels as $hotel) {
-                foreach($hotel as $item => $value) {
-                    ?>
+                <th scope="col"><?php echo $hotelProperty ?></th>
 
-                    <td><?php echo $value?></td>
-
-
-                        
-                    <?php
-                    }
-                ?>
-                
-            </tr>
-                
             <?php
             }
-            
+            ?>
+        </thead>
+        <tbody>
+            <?php
+
+
+            foreach ($filteredHotels as $hotel) {
+
+                echo "<tr>";
+
+                foreach ($hotel as $item => $value) {
+
+                    if ($item == 'parking') {
+                        if ($value == true) {
+                            echo "<td>OK</td>";
+                        } else {
+                            echo "<td>NO</td>";
+                        }
+                    } else {
+                        echo "<td>{$value}</td>";
+                    }
+                }
+
+                echo "</tr>";
+            }
+
             ?>
         </tbody>
     </table>
-    
-    
 
 
- 
-    
+
+
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
+
 </html>
 
 
